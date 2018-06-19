05f83663893973ea63edc4d4372cb42be28a160d
Bethany Kish <bethany.kish@digital.hmrc.gov.uk>
2018-06-19 15:49:03 +0100

    Implement filter, new test for Head and other changes from the session

--- src/main/scala/Head.scala
+++ src/main/scala/Head.scala
@@ -1,5 +1,27 @@
 package reinvent
 
 object Head {
-  def apply(input: List[Int]): Int = ???
+  def apply[A](input: List[A]): A = input match {
+    case Nil => throw new RuntimeException("Head of empty list")
+    case (h::_) => h
+  }
 }
+
+object Filter {
+  def apply[A](items: List[A], predicate: A => Boolean): List[A] = {
+    def applyFilter(items: List[A], accumulator: List[A]) : List[A] = {
+
+      if (items.isEmpty)  return accumulator
+
+      else if (predicate(items.head)) {
+        applyFilter(items.tail, accumulator :+ items.head)
+      }
+
+      else {
+        applyFilter(items.tail, accumulator)
+      }
+    }
+    applyFilter(items, List())
+  }
+}
+
--- src/test/scala/HeadTest.scala
+++ src/test/scala/HeadTest.scala
@@ -16,4 +16,36 @@ class HeadTest extends FunSuite with Matchers with PropertyChecks {
       }
     }
   }
+
+  test("Head should be first element of input"){
+    forAll{ input: List[Int] =>
+      whenever (input.nonEmpty){
+        input(0) == Head(input) should be (true)
+      }
+    }
+  }
+
 }
+
+class FilterTest extends FunSuite with Matchers with PropertyChecks {
+
+  test("Filter should return a list smaller or equal in size to the input") {
+    forAll { (input: List[Int], pred: Boolean) => {
+      Filter(input, { X: Int => pred }).size <= input.size} should be(true)
+    }
+  }
+
+
+  test("Filter should return the input if the predicate is always true") {
+    forAll { input: List[Int] =>
+      Filter(input, { X: Int => true }) should be(input)
+    }
+  }
+
+  test("Filter should return an empty list if the predicate is always false") {
+    forAll { input: List[Int] =>
+      Filter(input, { X: Int => false }) should be(Nil)
+    }
+  }
+}
+
